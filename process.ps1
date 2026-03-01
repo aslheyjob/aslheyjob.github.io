077f454909ad97b476b4ff90322d350fb7b862b1
aslheyP <aslheyjob@gmail.com>
2026-03-01 01:50:25 -0300

    feat: Add product data from CSV and introduce scripts to automate its integration into products.js.

--- process.ps1
+++ process.ps1
@@ -1,11 +1,11 @@
-$headers = "Col0","Name","Col2","Col3","Col4","Col5","Col6","Col7","ShortLink"
+$headers = "Col0", "Name", "Col2", "Col3", "Col4", "Col5", "Col6", "Col7", "ShortLink"
 $csvPath = "c:\Users\Val\Desktop\aslhey\aslheyjob.github.io\data.csv"
 $jsPath = "c:\Users\Val\Desktop\aslhey\aslheyjob.github.io\js\products.js"
 
 $csv = Import-Csv -Path $csvPath -Header $headers -Encoding UTF8
 
 $lines = @()
-$current_id = 254
+$current_id = 354
 foreach ($row in $csv) {
     if (-not [string]::IsNullOrWhiteSpace($row.Name)) {
         $name = $row.Name.Replace('"', '\"')
@@ -33,11 +33,11 @@ $insertText = $lines -join ",`r`n"
 $content = [System.IO.File]::ReadAllText($jsPath)
 $lastIdx = $content.LastIndexOf("];")
 if ($lastIdx -ge 0) {
-    # It might be `\n];` or just `];`. We will insert before it.
     # Check if there's an existing object list just above
     $newContent = $content.Substring(0, $lastIdx) + ",`r`n" + $insertText + "`r`n];"
     [System.IO.File]::WriteAllText($jsPath, $newContent, [System.Text.Encoding]::UTF8)
     Write-Host "Added $($lines.Count) products."
-} else {
+}
+else {
     Write-Host "Could not find '];' in $jsPath"
 }
